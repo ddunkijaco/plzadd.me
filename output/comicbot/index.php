6589e81e44dd00e93c534ad1e3cf90e101bf7dc2
Daniel Dunkijacobsnolten <daniel@danieldjn.com>
2014-04-24 11:08:30 -0700

    Change pisg time index and comicbot link styles

--- output/comicbot/index.php
+++ output/comicbot/index.php
@@ -1,43 +1,10 @@
 <html>
-<style type="text/css">
-body{text-align:center;background-color: #647684;}
-a
-{
-    text-decoration: none;
-}
-a:link
-{
-    color: #bdcddd;
-}
-a:visited
-{
-    color: #bdcddd;
-}
-a:hover
-{
-    color: #bdcddd;
-    text-decoration: underline;
-    font-weight: none;
-}
-a.background
-{
-    color: none;
-    text-decoration: none;
-}
-a.background:link
-{
-    color: #314151;
-}
-a.background:visited
-{
-    color: #415161;
-}
-a.background:hover
-{
-    color: #003c60;
-    text-decoration: underline;
-}
+<head>
+<link rel="stylesheet" type="text/css" href="../../pisg/layout/justgrey.css">
+<style  type="text/css">
+body{text-align:center}
 </style>
+</head>
 <body>
 <script src="jquery.js" type="text/javascript"></script>
 <script src="jquery.lazyload.js" type="text/javascript"></script>
@@ -59,24 +26,26 @@ a.background:hover
 		});
     });
 </script>
-<a class="comic" href="#">All</a> | 
-<a class="comic" href="#">Alien</a> | 
-<a class="comic" href="#">BeanieBears</a> | 
-<a class="comic" href="#">Dane</a> | 
-<a class="comic" href="#">DCStrip</a> | 
-<a class="comic" href="#">Dino</a> | 
-<a class="comic" href="#">Kittens</a> | 
-<a class="comic" href="#">Macs</a> | 
-<a class="comic" href="#">Minecraft</a> | 
-<a class="comic" href="#">Peanuts</a> | 
-<a class="comic" href="#">Pies</a> | 
-<a class="comic" href="#">Rifle</a> | 
-<a class="comic" href="#">Rock</a> | 
-<a class="comic" href="#">RollingStones</a> | 
-<a class="comic" href="#">Skimask</a> | 
-<a class="comic" href="#">Stickfig</a> | 
-<a class="comic" href="#">Tractor</a> | 
-<a class="comic" href="#">Yeah</a> | 
+<span class="title">
+	<a class="comic" href="#">All</a> | 
+	<a class="comic" href="#">Alien</a> | 
+	<a class="comic" href="#">BeanieBears</a> | 
+	<a class="comic" href="#">Dane</a> | 
+	<a class="comic" href="#">DCStrip</a> | 
+	<a class="comic" href="#">Dino</a> | 
+	<a class="comic" href="#">Kittens</a> | 
+	<a class="comic" href="#">Macs</a> | 
+	<a class="comic" href="#">Minecraft</a> | 
+	<a class="comic" href="#">Peanuts</a> | 
+	<a class="comic" href="#">Pies</a> | 
+	<a class="comic" href="#">Rifle</a> | 
+	<a class="comic" href="#">Rock</a> | 
+	<a class="comic" href="#">RollingStones</a> | 
+	<a class="comic" href="#">Skimask</a> | 
+	<a class="comic" href="#">Stickfig</a> | 
+	<a class="comic" href="#">Tractor</a> | 
+	<a class="comic" href="#">Yeah</a><br/><br/>
+</span>
 <div id="content">
 <?php
 $files = glob('../comicbot/cartoon-*.png');
